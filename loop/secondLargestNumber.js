e00e86c1e93c602115651ea67fc340d196fe9f78
ApnaCollageYoutube <anandsinha975@gmail.com>
2026-03-23 19:54:11 +0530

    fix logic for finding the second largest number in an array

--- loop/secondLargestNumber.js
+++ loop/secondLargestNumber.js
@@ -7,13 +7,13 @@ const secondLargestNumber = (arr) => {
             secondLargestNumber = firstLargestNumber;
             firstLargestNumber = arr[i];
         }
-        else if(arr[i] > secondLargestNumber){
+        else if(arr[i] > secondLargestNumber && !firstLargestNumber){
             secondLargestNumber = arr[i]
         }
     }
     return secondLargestNumber;
 }
 
-let arr = [2, 5, 7, 6, 5, 8, 7];
+let arr = [2, 5, 7, 6, 5, 8, 7, 8];
 let result = secondLargestNumber(arr);
 console.log(result)
